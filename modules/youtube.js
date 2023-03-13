382cfb47fa5f0ce6a20da0cbdfaaf50fb11ec469
Daniel Giustini Perez <danielgiustiniperez@gmail.com>
2023-03-13 10:24:25 +0100

    Clean code in index.js

--- modules/youtube.js
+++ modules/youtube.js
@@ -1,87 +1,218 @@
-import puppeteer from "../utilities/puppeteer.js";
+import puppeteer from "puppeteer";
+
+import monthToNumber from "../utilities/month-to-number.js";
 import wait from "../utilities/wait.js";
-import write from "../utilities/write.js";
 
-export default async () => {
-  const browser = await puppeteer(false);
-  console.log("1. Browser launched");
+import announcement from "../models/announcement.js";
 
+const getItems = async () => {
+  // Launch browser
+  const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
-  await page.goto("https://blog.youtube/news-and-events");
-  console.log("2. Page opened");
+  console.log("YouTube: Browser launched");
 
-  let loadMoreButton = await page.waitForSelector(
-    "button.yt-latest-articles__load-more"
-  );
+  // Open newsroom
+  await page.goto("https://blog.youtube/news-and-events");
+  await wait(5000);
+  console.log("YouTube: Newsroom opened");
+
+  // Load entire item history
+  let loadMoreButton = await page.$("button.yt-latest-articles__load-more");
   while (loadMoreButton) {
     await loadMoreButton.evaluate((el) => el.click());
-    await wait(3000);
-    const lastChild = await page.$("li.yt-latest-articles__item:last-child");
-    const lastChildTimeElement = lastChild
-      ? await lastChild.$("time.yt-latest-articles__date")
-      : null;
-    const lastChildTimeText = lastChildTimeElement
-      ? await lastChildTimeElement.evaluate((el) => el.textContent)
-      : "";
-    const lastChildTime =
-      lastChildTimeText && typeof lastChildTimeText === "string"
-        ? lastChildTimeText.trim()
-        : "";
-    console.log(lastChildTime);
-    if (lastChildTime !== "Jul.07.2005") {
-      const newLoadMoreButton = await page.waitForSelector(
-        "button.yt-latest-articles__load-more"
+    await wait(5000);
+    const lastItemTime = await page
+      .$("li.yt-latest-articles__item:last-child")
+      .then((res) => {
+        return res ? res.evaluate((el) => el.textContent) : "";
+      })
+      .then((res) => {
+        return res && typeof res === "string" ? res.trim() : "";
+      })
+      .catch((error) => {
+        console.log(error);
+        return "";
+      });
+    loadMoreButton =
+      lastItemTime !== "Jul.07.2005"
+        ? await page
+            .$("button.yt-latest-articles__load-more")
+            .then((res) => res || null)
+            .catch(() => null)
+        : null;
+  }
+  console.log("YouTube: Item history loaded");
+
+  const data = [];
+  const items = await page.$$("li.yt-latest-articles__item");
+  for (const item of items) {
+    const category = await item
+      .$("p.yt-latest-articles__category")
+      .then((res) => {
+        return res ? res.evaluate((el) => el.textContent) : null;
+      })
+      .then((res) => {
+        return res && typeof res === "string" ? res.trim() : "";
+      })
+      .catch((error) => {
+        console.log(error);
+        return "";
+      });
+    const title = await item
+      .$("h3.yt-latest-articles__title")
+      .then((res) => {
+        return res ? res.evaluate((el) => el.textContent) : null;
+      })
+      .then((res) => {
+        return res && typeof res === "string" ? res.trim() : "";
+      })
+      .catch((error) => {
+        console.log(error);
+        return "";
+      });
+    const date = await item
+      .$("time.yt-latest-articles__date")
+      .then((res) => {
+        return res ? res.evaluate((el) => el.textContent) : null;
+      })
+      .then((res) => {
+        return res && typeof res === "string" ? res.trim() : "";
+      })
+      .catch((error) => {
+        console.log(error);
+        return "";
+      });
+    const link = await item
+      .$("a.yt-latest-articles__link")
+      .then((res) => {
+        return res ? res.evaluate((el) => el.getAttribute("href")) : null;
+      })
+      .then((res) => {
+        return res && typeof res === "string" ? res.trim() : "";
+      })
+      .catch((error) => {
+        console.log(error);
+        return "";
+      });
+    const dataItem = { category, title, date, link };
+    data.push(dataItem);
+  }
+  console.log("YouTube: Data extracted from items");
+
+  // Close browser
+  await browser.close();
+  console.log("YouTube: Browser closed");
+
+  // Return data
+  return data;
+};
+
+const getItemsData = async (items) => {
+  console.log(items);
+  // Launch browser
+  const browser = await puppeteer.launch({ headless: true });
+  const page = await browser.newPage();
+  console.log("YouTube: Browser launched");
+
+  // Extract item data
+  const n = items.length;
+  const data = [];
+  for (let i = 0; i < n; i++) {
+    try {
+      console.log(`YouTube: ${i + 1}/${n}`);
+      const item = items[i];
+      await page.goto(item.link);
+      await wait(5000);
+      const year = item.date ? +item.date.split(".")[2] : null;
+      const month = item.date ? monthToNumber(item.date.split(".")[0]) : "";
+      const day = item.date ? +item.date.split(".")[1] : null;
+      const date = new Date(year, month, day);
+      const title = item.title;
+      const description = await page
+        .$('head meta[name="description"]')
+        .then((res) => {
+          return res ? res.evaluate((el) => el.getAttribute("content")) : "";
+        })
+        .then((res) => {
+          return res && typeof res === "string" ? res.trim() : "";
+        })
+        .catch((error) => {
+          console.log(error);
+          return "";
+        });
+      const tagElements = await page.$$("ul.yt-article-rel-tags__list li");
+      const tags = [];
+      for (const tagElement of tagElements) {
+        const tag = await tagElement
+          .evaluate((el) => el.textContent)
+          .then((res) => {
+            return res && typeof res === "string" ? res.trim() : "";
+          })
+          .catch(() => {
+            console.log(error);
+            return "";
+          });
+        if (tag) {
+          tags.push(tag);
+        }
+      }
+      if (item.category && !tags.includes(item.category)) {
+        tags.push(item.category);
+      }
+      const author = await page
+        .$("ul.yt-articlepage__page-title-container-author")
+        .then((res) => {
+          return res ? res.evaluate((el) => el.textContent) : "";
+        })
+        .then((res) => {
+          return res && typeof res === "string"
+            ? res.trim().slice(0, 3) === "By "
+              ? res
+                  .slice(3)
+                  .replace(item.date, "")
+                  .split("\n")
+                  .map((x) => x.trim())
+                  .filter((x) => x)
+                  .join(" - ")
+              : res
+                  .replace(item.date, "")
+                  .split("\n")
+                  .map((x) => x.trim())
+                  .filter((x) => x)
+                  .join(" - ")
+            : "";
+        })
+        .catch((error) => {
+          console.log(error);
+          return "";
+        });
+      const url = item.link;
+      const newAnnouncement = announcement(
+        "YouTube",
+        date,
+        title,
+        description,
+        tags,
+        author,
+        url
       );
-      loadMoreButton = newLoadMoreButton || null;
-    } else {
-      loadMoreButton = null;
+      if (newAnnouncement) {
+        data.push(newAnnouncement);
+      } else {
+        throw new Error("YouTube: Invalid data");
+      }
+    } catch (error) {
+      console.log(error);
     }
   }
-  console.log("3. Blog elements loaded");
+  console.log("YouTube: Item data extracted");
 
-  const items = [];
-  const liElements = await page.$$("li.yt-latest-articles__item");
-  for (const liElement of liElements) {
-    const categoryElement = await liElement.$("p.yt-latest-articles__category");
-    const categoryText = categoryElement
-      ? await categoryElement.evaluate((el) => el.textContent)
-      : "";
-    const category =
-      categoryText && typeof categoryText === "string"
-        ? categoryText.toLowerCase().trim()
-        : "";
-    const titleElement = await liElement.$("h3.yt-latest-articles__title");
-    const titleText = titleElement
-      ? await titleElement.evaluate((el) => el.textContent)
-      : "";
-    const title =
-      titleText && typeof titleText === "string" ? titleText.trim() : "";
-    const dateElement = await liElement.$("time.yt-latest-articles__date");
-    const dateText = dateElement
-      ? await dateElement.evaluate((el) => el.textContent)
-      : "";
-    const date =
-      dateText && typeof dateText === "string" ? dateText.trim() : "";
-    const linkElement = await liElement.$("a.yt-latest-articles__link");
-    const linkText = linkElement
-      ? await linkElement.evaluate((el) => el.getAttribute("href"))
-      : "";
-    const link =
-      linkText && typeof linkText === "string"
-        ? linkText.toLowerCase().trim()
-        : "";
-    const item = { category, title, date, link };
-    items.push(item);
-  }
-  console.log("4. Blog elements parsed");
-
-  const fileName = "./output/youtube-" + Date.now() + ".json";
-  const fileData = JSON.stringify(items);
-  await write(fileName, fileData);
-  console.log("5. Scrape process completed");
-
+  // Close browser
   await browser.close();
-  console.log("6. Browser closed");
+  console.log("YouTube: Browser closed");
 
-  return;
+  // Return data
+  return data;
 };
+
+export default { getItems, getItemsData };
