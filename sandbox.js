496e9cc43d94dd4564f9e4f78e19b222c7361c1c
Daniel Giustini Perez <danielgiustiniperez@gmail.com>
2023-03-14 09:48:17 +0100

    Rename full text data files

--- sandbox.js
+++ sandbox.js
@@ -1,1 +1,13 @@
+import read from "./utilities/read.js";
 
+const init = async () => {
+  const input = await read("./output/facebook_text.json")
+    .then((res) => JSON.parse(res))
+    .catch((error) => {
+      console.log(error);
+      return [];
+    });
+  console.log(input);
+};
+
+init();
