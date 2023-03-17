1a8eef7907e24e461ffc85ec04e588ce61379a34
Daniel Giustini Perez <danielgiustiniperez@gmail.com>
2023-03-17 14:44:04 +0100

    Generate text similarities between data

--- index.js
+++ index.js
@@ -13,135 +13,108 @@ import youtube from "./modules/youtube.js";
 import read from "./utilities/read.js";
 import write from "./utilities/write.js";
 
-const gatherPosts = async () => {
+const getPosts = async () => {
   // Facebook
-  // const facebookItems = await facebook.getItems();
-  // const facebookItemsData = await facebook.getItemsData(facebookItems);
-  // const facebookFileName = "./output/facebook-" + Date.now() + ".json";
-  // const facebookFileData = JSON.stringify(facebookItemsData);
-  // await write(facebookFileName, facebookFileData);
+  const facebookItems = await facebook.getItems();
+  const facebookItemsData = await facebook.getItemsData(facebookItems);
+  const facebookFileName = "./output/01-data-basic-facebook-.json";
+  const facebookFileData = JSON.stringify(facebookItemsData);
+  await write(facebookFileName, facebookFileData);
 
   // // Instagram
-  // const instagramItems = await instagram.getItems();
-  // const instagramItemsData = await instagram.getItemsData(instagramItems);
-  // const instagramFileName = "./output/instagram-" + Date.now() + ".json";
-  // const instagramFileData = JSON.stringify(instagramItemsData);
-  // await write(instagramFileName, instagramFileData);
+  const instagramItems = await instagram.getItems();
+  const instagramItemsData = await instagram.getItemsData(instagramItems);
+  const instagramFileName = "./output/01-data-basic-instagram-.json";
+  const instagramFileData = JSON.stringify(instagramItemsData);
+  await write(instagramFileName, instagramFileData);
 
   // // LinkedIn
-  // const linkedinItems = await linkedin.getItems();
-  // const linkedinItemsData = await linkedin.getItemsData(linkedinItems);
-  // const linkedinFileName = "./output/linkedin-" + Date.now() + ".json";
-  // const linkedinFileData = JSON.stringify(linkedinItemsData);
-  // await write(linkedinFileName, linkedinFileData);
+  const linkedinItems = await linkedin.getItems();
+  const linkedinItemsData = await linkedin.getItemsData(linkedinItems);
+  const linkedinFileName = "./output/01-data-basic-linkedin-.json";
+  const linkedinFileData = JSON.stringify(linkedinItemsData);
+  await write(linkedinFileName, linkedinFileData);
 
   // // Pinterest
-  // const pinterestItems = await pinterest.getItems();
-  // const pinterestItemsData = await pinterest.getItemsData(pinterestItems);
-  // const pinterestFileName = "./output/pinterest-" + Date.now() + ".json";
-  // const pinterestFileData = JSON.stringify(pinterestItemsData);
-  // await write(pinterestFileName, pinterestFileData);
+  const pinterestItems = await pinterest.getItems();
+  const pinterestItemsData = await pinterest.getItemsData(pinterestItems);
+  const pinterestFileName = "./output/01-data-basic-pinterest-.json";
+  const pinterestFileData = JSON.stringify(pinterestItemsData);
+  await write(pinterestFileName, pinterestFileData);
 
   // // Reddit
-  // const redditItems = await reddit.getItems();
-  // const redditItemsData = await reddit.getItemsData(redditItems);
-  // const redditFileName = "./output/reddit-" + Date.now() + ".json";
-  // const redditFileData = JSON.stringify(redditItemsData);
-  // await write(redditFileName, redditFileData);
+  const redditItems = await reddit.getItems();
+  const redditItemsData = await reddit.getItemsData(redditItems);
+  const redditFileName = "./output/01-data-basic-reddit-.json";
+  const redditFileData = JSON.stringify(redditItemsData);
+  await write(redditFileName, redditFileData);
 
   // // Snapchat
-  // const snapchatItems = await snapchat.getItems();
-  // const snapchatItemsData = await snapchat.getItemsData(snapchatItems);
-  // const snapchatFileName = "./output/snapchat-" + Date.now() + ".json";
-  // const snapchatFileData = JSON.stringify(snapchatItemsData);
-  // await write(snapchatFileName, snapchatFileData);
+  const snapchatItems = await snapchat.getItems();
+  const snapchatItemsData = await snapchat.getItemsData(snapchatItems);
+  const snapchatFileName = "./output/01-data-basic-snapchat-.json";
+  const snapchatFileData = JSON.stringify(snapchatItemsData);
+  await write(snapchatFileName, snapchatFileData);
 
   // // Stack Overflow
-  // const stackoverflowItems = await stackoverflow.getItems();
-  // const stackoverflowItemsData = await stackoverflow.getItemsData(
-  //   stackoverflowItems
-  // );
-  // const stackoverflowFileName =
-  //   "./output/stackoverflow-" + Date.now() + ".json";
-  // const stackoverflowFileData = JSON.stringify(stackoverflowItemsData);
-  // await write(stackoverflowFileName, stackoverflowFileData);
+  const stackoverflowItems = await stackoverflow.getItems();
+  const stackoverflowItemsData = await stackoverflow.getItemsData(
+    stackoverflowItems
+  );
+  const stackoverflowFileName = "./output/01-data-basic-stackoverflow-.json";
+  const stackoverflowFileData = JSON.stringify(stackoverflowItemsData);
+  await write(stackoverflowFileName, stackoverflowFileData);
 
   // // TikTok
-  // const tiktokItems = await tiktok.getItems();
-  // const tiktokItemsData = await tiktok.getItemsData(tiktokItems);
-  // const tiktokFileName = "./output/tiktok-" + Date.now() + ".json";
-  // const tiktokFileData = JSON.stringify(tiktokItemsData);
-  // await write(tiktokFileName, tiktokFileData);
+  const tiktokItems = await tiktok.getItems();
+  const tiktokItemsData = await tiktok.getItemsData(tiktokItems);
+  const tiktokFileName = "./output/01-data-basic-tiktok-.json";
+  const tiktokFileData = JSON.stringify(tiktokItemsData);
+  await write(tiktokFileName, tiktokFileData);
 
   // // Twitter
-  // const twitterItems = await twitter.getItems();
-  // const twitterItemsData = await twitter.getItemsData(twitterItems);
-  // const twitterFileName = "./output/twitter-" + Date.now() + ".json";
-  // const twitterFileData = JSON.stringify(twitterItemsData);
-  // await write(twitterFileName, twitterFileData);
+  const twitterItems = await twitter.getItems();
+  const twitterItemsData = await twitter.getItemsData(twitterItems);
+  const twitterFileName = "./output/01-data-basic-twitter-.json";
+  const twitterFileData = JSON.stringify(twitterItemsData);
+  await write(twitterFileName, twitterFileData);
 
   // // whatsApp
-  // const whatsappItems = await whatsapp.getItems();
-  // const whatsappItemsData = await whatsapp.getItemsData(whatsappItems);
-  // const whatsappFileName = "./output/whatsapp-" + Date.now() + ".json";
-  // const whatsappFileData = JSON.stringify(whatsappItemsData);
-  // await write(whatsappFileName, whatsappFileData);
+  const whatsappItems = await whatsapp.getItems();
+  const whatsappItemsData = await whatsapp.getItemsData(whatsappItems);
+  const whatsappFileName = "./output/01-data-basic-whatsapp-.json";
+  const whatsappFileData = JSON.stringify(whatsappItemsData);
+  await write(whatsappFileName, whatsappFileData);
 
   // // YouTube
-  // const youtubeItems = await youtube.getItems();
-  // const youtubeItemsData = await youtube.getItemsData(youtubeItems);
-  // const youtubeFileName = "./output/youtube-" + Date.now() + ".json";
-  // const youtubeFileData = JSON.stringify(youtubeItemsData);
-  // await write(youtubeFileName, youtubeFileData);
+  const youtubeItems = await youtube.getItems();
+  const youtubeItemsData = await youtube.getItemsData(youtubeItems);
+  const youtubeFileName = "./output/01-data-basic-youtube-.json";
+  const youtubeFileData = JSON.stringify(youtubeItemsData);
+  await write(youtubeFileName, youtubeFileData);
 
   // // JSON Export
-  // const allItemsData = [
-  //   ...facebookItemsData,
-  //   ...instagramItemsData,
-  //   ...pinterestItemsData,
-  //   ...redditItemsData,
-  //   ...snapchatItemsData,
-  //   ...stackoverflowItemsData,
-  //   ...tiktokItemsData,
-  //   ...twitterItemsData,
-  //   ...whatsappItemsData,
-  //   ...youtubeItemsData,
-  // ].sort((a, b) => b.date - a.date);
-  // const allFileName = "./output/_data-" + Date.now() + ".json";
-  // const allFileData = JSON.stringify(allItemsData);
-  // await write(allFileName, allFileData);
-
-  // // XLSX Export
-  // const xlsxData = allItemsData.map((item) => {
-  //   return [
-  //     item.platform,
-  //     new Date(item.date).toISOString().slice(0, 10),
-  //     item.title,
-  //     item.description,
-  //     item.tags.join(", "),
-  //     item.author,
-  //     item.url,
-  //   ];
-  // });
-  // xlsxData.splice(0, 0, [
-  //   "Platform",
-  //   "Date",
-  //   "Title",
-  //   "Description",
-  //   "Tags",
-  //   "Author",
-  //   "URL",
-  // ]);
-  // const workbook = XLSX.utils.book_new();
-  // const worksheet = XLSX.utils.aoa_to_sheet(xlsxData);
-  // XLSX.utils.book_append_sheet(workbook, worksheet, "Sheet1");
-  // XLSX.writeFile(workbook, "./output/_data-" + Date.now() + ".xlsx");
+  const allItemsData = [
+    ...facebookItemsData,
+    ...instagramItemsData,
+    ...pinterestItemsData,
+    ...redditItemsData,
+    ...snapchatItemsData,
+    ...stackoverflowItemsData,
+    ...tiktokItemsData,
+    ...twitterItemsData,
+    ...whatsappItemsData,
+    ...youtubeItemsData,
+  ].sort((a, b) => b.date - a.date);
+  const allFileName = "./output/01-data-basic.json";
+  const allFileData = JSON.stringify(allItemsData);
+  await write(allFileName, allFileData);
 
   process.exit();
 };
 
 const getPostsText = async () => {
-  const all = await read("./output/_data.json")
+  const all = await read("./output/01-data-basic.json")
     .then((res) => JSON.parse(res))
     .catch(() => []);
 
@@ -153,7 +126,7 @@ const getPostsText = async () => {
       console.log(error);
       return [];
     });
-  const facebookFileName = "./output/facebook_text-" + Date.now() + ".json";
+  const facebookFileName = "./output/02-data-complete-facebook.json";
   const facebookFileData = JSON.stringify(facebookOutput);
   await write(facebookFileName, facebookFileData);
 
@@ -165,7 +138,7 @@ const getPostsText = async () => {
       console.log(error);
       return [];
     });
-  const instagramFileName = "./output/instagram_text-" + Date.now() + ".json";
+  const instagramFileName = "./output/02-data-complete-instagram.json";
   const instagramFileData = JSON.stringify(instagramOutput);
   await write(instagramFileName, instagramFileData);
 
@@ -177,7 +150,7 @@ const getPostsText = async () => {
       console.log(error);
       return [];
     });
-  const linkedinFileName = "./output/linkedin_text-" + Date.now() + ".json";
+  const linkedinFileName = "./output/02-data-complete-linkedin.json";
   const linkedinFileData = JSON.stringify(linkedinOutput);
   await write(linkedinFileName, linkedinFileData);
 
@@ -189,7 +162,7 @@ const getPostsText = async () => {
       console.log(error);
       return [];
     });
-  const pinterestFileName = "./output/pinterest_text-" + Date.now() + ".json";
+  const pinterestFileName = "./output/02-data-complete-pinterest.json";
   const pinterestFileData = JSON.stringify(pinterestOutput);
   await write(pinterestFileName, pinterestFileData);
 
@@ -199,7 +172,7 @@ const getPostsText = async () => {
     console.log(error);
     return [];
   });
-  const redditFileName = "./output/reddit_text-" + Date.now() + ".json";
+  const redditFileName = "./output/02-data-complete-reddit.json";
   const redditFileData = JSON.stringify(redditOutput);
   await write(redditFileName, redditFileData);
 
@@ -211,7 +184,7 @@ const getPostsText = async () => {
       console.log(error);
       return [];
     });
-  const snapchatFileName = "./output/snapchat_text-" + Date.now() + ".json";
+  const snapchatFileName = "./output/02-data-complete-snapchat.json";
   const snapchatFileData = JSON.stringify(snapchatOutput);
   await write(snapchatFileName, snapchatFileData);
 
@@ -223,7 +196,7 @@ const getPostsText = async () => {
       console.log(error);
       return [];
     });
-  const stackFileName = "./output/stackoverflow_text-" + Date.now() + ".json";
+  const stackFileName = "./output/02-data-complete-stackoverflow.json";
   const stackFileData = JSON.stringify(stackOutput);
   await write(stackFileName, stackFileData);
 
@@ -233,7 +206,7 @@ const getPostsText = async () => {
     console.log(error);
     return [];
   });
-  const tiktokFileName = "./output/tiktok_text-" + Date.now() + ".json";
+  const tiktokFileName = "./output/02-data-complete-tiktok.json";
   const tiktokFileData = JSON.stringify(tiktokOutput);
   await write(tiktokFileName, tiktokFileData);
 
@@ -245,7 +218,7 @@ const getPostsText = async () => {
       console.log(error);
       return [];
     });
-  const twitterFileName = "./output/twitter_text-" + Date.now() + ".json";
+  const twitterFileName = "./output/02-data-complete-twitter.json";
   const twitterFileData = JSON.stringify(twitterOutput);
   await write(twitterFileName, twitterFileData);
 
@@ -257,7 +230,7 @@ const getPostsText = async () => {
       console.log(error);
       return [];
     });
-  const whatsappFileName = "./output/whatsapp_text-" + Date.now() + ".json";
+  const whatsappFileName = "./output/02-data-complete-whatsapp.json";
   const whatsappFileData = JSON.stringify(whatsappOutput);
   await write(whatsappFileName, whatsappFileData);
 
@@ -269,12 +242,97 @@ const getPostsText = async () => {
       console.log(error);
       return [];
     });
-  const youtubeFileName = "./output/youtube_text-" + Date.now() + ".json";
+  const youtubeFileName = "./output/02-data-complete-youtube.json";
   const youtubeFileData = JSON.stringify(youtubeOutput);
   await write(youtubeFileName, youtubeFileData);
 
   process.exit();
 };
 
+const analyzeTextSimilarities = async () => {
+  const input = await read("./output/05-data-alphanumeric.json")
+    .then((res) => {
+      return JSON.parse(res);
+    })
+    .catch((error) => {
+      console.log(error);
+      return [];
+    });
+
+  const nItems = input.length;
+  const combinations = input.reduce((result, item, index, array) => {
+    const id = item.id;
+    if (index < nItems - 1) {
+      const remainingCombinations = array
+        .slice(index + 1)
+        .map((remainingItem) => [id, remainingItem.id]);
+      result.push(...remainingCombinations);
+    }
+    return result;
+  }, []);
+
+  const batchSize = 200000;
+  const nBatches = Math.ceil(combinations.length / batchSize);
+  let batchIndex = 5;
+
+  while (batchIndex < nBatches) {
+    const output = [];
+    const startIndex = batchIndex * batchSize;
+    const endIndex = startIndex + batchSize;
+    const batch = combinations.slice(startIndex, endIndex);
+    const nCombinations = batch.length;
+    for (let i = 0; i < nCombinations; i++) {
+      console.log(
+        "Batch " +
+          (batchIndex + 1) +
+          "/" +
+          nBatches +
+          " - Combination " +
+          (i + 1) +
+          "/" +
+          nCombinations
+      );
+      const combination = batch[i];
+      const id1 = combination[0];
+      const id2 = combination[1];
+      const item1 = input.find((item) => item.id === id1);
+      const item2 = input.find((item) => item.id === id2);
+      const words1 = item1.text.split(" ").filter((word) => word.length > 3);
+      const words2 = item2.text.split(" ").filter((word) => word.length > 3);
+      const length1 = words1.length;
+      const length2 = words2.length;
+      const union = length1 + length2;
+      const intersection =
+        length1 < length2
+          ? words1.filter((word) => words2.includes(word)).length
+          : words2.filter((word) => words1.includes(word)).length;
+      const jaccard = intersection / union;
+      const outputItem = {
+        ids: [id1, id2],
+        platforms: [item1.platform, item2.platform],
+        dates: [item1.date, item2.date],
+        tags: [item1.tags, item2.tags],
+        titles: [item1.title, item2.title],
+        textLengths: [length1, length2],
+        jaccardIndex: jaccard,
+      };
+      output.push(outputItem);
+    }
+    await write(
+      "./output/06-data-similarities-batch-" + (batchIndex + 1) + ".json",
+      JSON.stringify(output)
+    )
+      .then(() => {
+        console.log("Batch " + (batchIndex + 1) + " completed successfully.");
+      })
+      .catch((error) => {
+        console.log("Batch " + (batchIndex + 1) + " failed.");
+        console.log(error);
+      });
+    batchIndex++;
+  }
+};
+
 // gatherPosts();
 // getPostsText();
+// analyzeTextSimilarities();
--- sandbox.js
+++ sandbox.js
@@ -3,87 +3,51 @@ import read from "./utilities/read.js";
 import write from "./utilities/write.js";
 
 const init = async () => {
-  const input = await read("./output/05-data-alphanumeric.json")
-    .then((res) => {
-      return JSON.parse(res);
-    })
-    .catch((error) => {
-      console.log(error);
-      return [];
-    });
-
-  const nItems = input.length;
-  const combinations = input.reduce((result, item, index, array) => {
-    const id = item.id;
-    if (index < nItems - 1) {
-      const remainingCombinations = array
-        .slice(index + 1)
-        .map((remainingItem) => [id, remainingItem.id]);
-      result.push(...remainingCombinations);
-    }
-    return result;
-  }, []);
-
-  const batchSize = 200000;
-  const nBatches = Math.ceil(combinations.length / batchSize);
-  let batchIndex = 0;
-
-  while (batchIndex < nBatches) {
+  for (let i = 1; i <= 95; i++) {
+    const input = await read(
+      "./output/06-data-similarities-batch-" + i + ".json"
+    )
+      .then((res) => JSON.parse(res))
+      .catch((error) => {
+        console.log(error);
+        return [];
+      });
+    const n = input.length;
+    const sampleIndexes = [];
+    const sampleSizePercentage = 0.0005;
+    const sampleSize = Math.round(sampleSizePercentage * n);
     const output = [];
-    const startIndex = batchIndex * batchSize;
-    const endIndex = startIndex + batchSize;
-    const batch = combinations.slice(startIndex, endIndex);
-    const nCombinations = batch.length;
-    for (let i = 0; i < nCombinations; i++) {
-      console.log(
-        "Batch " +
-          (batchIndex + 1) +
-          "/" +
-          nBatches +
-          " - Combination " +
-          (i + 1) +
-          "/" +
-          nCombinations
-      );
-      const combination = batch[i];
-      const id1 = combination[0];
-      const id2 = combination[1];
-      const item1 = input.find((item) => item.id === id1);
-      const item2 = input.find((item) => item.id === id2);
-      const words1 = item1.text.split(" ").filter((word) => word.length > 3);
-      const words2 = item2.text.split(" ").filter((word) => word.length > 3);
-      const length1 = words1.length;
-      const length2 = words2.length;
-      const union = length1 + length2;
-      const intersection =
-        length1 < length2
-          ? words1.filter((word) => words2.includes(word)).length
-          : words2.filter((word) => words1.includes(word)).length;
-      const jaccard = intersection / union;
-      const outputItem = {
-        ids: [id1, id2],
-        platforms: [item1.platform, item2.platform],
-        dates: [item1.date, item2.date],
-        tags: [item1.tags, item2.tags],
-        titles: [item1.title, item2.title],
-        textLengths: [length1, length2],
-        jaccardIndex: jaccard,
-      };
-      output.push(outputItem);
+    for (let j = 0; j < sampleSize; j++) {
+      let sampleIndex = Math.floor(Math.random() * n);
+      while (sampleIndexes.includes(sampleIndex)) {
+        sampleIndex = Math.floor(Math.random() * n);
+      }
+      output.push(input[sampleIndex]);
+      sampleIndexes.push(sampleIndex);
     }
     await write(
-      "./output/06-data-similarities-batch-" + (batchIndex + 1) + ".json",
+      "./output/06-data-similarities-samples/06-data-similarities-sample-" +
+        i +
+        ".json",
       JSON.stringify(output)
-    )
-      .then(() => {
-        console.log("Batch " + (batchIndex + 1) + " completed successfully.");
-      })
-      .catch((error) => {
-        console.log("Batch " + (batchIndex + 1) + " failed.");
-        console.log(error);
-      });
-    batchIndex++;
+    );
   }
+  // for (let i = 1; i <= 95; i++) {
+  //   console.log("Batch " + i);
+  //   const input = await read(
+  //     "./output/06-data-similarities-batch-" + i + ".json"
+  //   )
+  //     .then((res) => JSON.parse(res))
+  //     .catch((error) => {
+  //       console.log(error);
+  //       return [];
+  //     });
+  //   for (const item of input) {
+  //     output.push(item);
+  //   }
+  // }
+  // console.log(output.length);
+  // await write("./output/06-data-similarities.json", JSON.stringify(output));
 };
 
 init();
